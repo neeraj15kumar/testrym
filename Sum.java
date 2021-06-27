2f56c9c9924a347788238cf29876b44f60157150
Neeraj Kumar <neeraj15kumar@gmail.com>
2021-06-27 13:02:04 +0530

    third commit

--- Sum.java
+++ Sum.java
@@ -3,7 +3,7 @@ public class Sum{
     public static void main(String args[]){
         int a = 10;
         int b = 20;
-        System.out.println("Sum of a + b = "+(a+b));
+        System.out.println("Sum of a + b == "+(a+b));
     }
 
 }
